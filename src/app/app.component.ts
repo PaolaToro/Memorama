44343772b930d7e501c0609c03db3c74911c34fe
Paola Toro <paotoro94@gmail.com>
2020-03-01 23:40:28 -0300

    Update cards logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { CardComponent } from './card/card.component';
 
+import shuffleArray from './helpers/shuffle-array';
+
 const BASE_ASSET_PATH = 'assets';
+
+const COVER_ASSET = 'tapa.jpg';
 
 // this variable is readonly because it shouldn't mutate the
 // content of the array
@@ -15,7 +19,7 @@ const HEROES_ASSETS: readonly string[] = [
   'spiderman.jpg',
   'strange.jpg',
   'thanos.jpg',
-  'thor.jpg',
+  'thor.jpg'
 ];
 
 interface Card {
@@ -30,6 +34,8 @@ interface Card {
 })
 export class AppComponent {
   title = 'Memorama';
+
+  // The size of the `HEROES_ASSETS` should be the half of `rows` * `columns`
   rows = 5;
   columns = 4;
 
@@ -42,13 +48,32 @@ export class AppComponent {
   generateCards = (): Card[][] => {
     const matrix = [];
 
+    // The card assets are the doubles of cards
+    const cardsAssets = [].concat(HEROES_ASSETS, HEROES_ASSETS);
+
+    // The carsd is a array of resources where the back is a constant
+    // and the front is an image of a heroe
+    const cards = new Array(this.rows * this.columns)
+        .fill({})
+        .map((_, i) => {
+          return ({
+            back: `${BASE_ASSET_PATH}/${COVER_ASSET}`,
+            front: `${BASE_ASSET_PATH}/${cardsAssets[i]}`
+          });
+        });
+
+    // The shuffle cards are the same cards but randomly mixed
+    const shuffledCards = shuffleArray(cards);
+
+    // We use the control index for know where in the shuffle cards we are
+    let controlIndex = 0;
     for (let i = 0; i < this.rows; i++) {
       if (!matrix[i]) {
         matrix[i] = [];
       }
-
       for (let j = 0; j < this.columns; j++) {
-        matrix[i][j] = { back: `${BASE_ASSET_PATH}/tapa.jpg`, front: `${BASE_ASSET_PATH}/batman.jpg` };
+        matrix[i][j] = shuffledCards[controlIndex];
+        controlIndex++;
       }
     }
 

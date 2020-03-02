12234937b5499ed1aa20e2cdd95743798b06ba26
Paola Toro <paotoro94@gmail.com>
2020-03-01 22:53:12 -0300

    Update card component handle own source variables

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+const BASE_ASSET_PATH = 'assets';
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
 
   active: boolean;
 
+  back: string;
+  front: string;
+
   constructor() {
     this.active = false;
+
+    this.back = `${BASE_ASSET_PATH}/tapa.jpg`;
+    this.front = `${BASE_ASSET_PATH}/batman.jpg`;
   }
 
   ngOnInit(): void {

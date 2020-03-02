e078f7a12a0dbfb3cf377b491e6f176102ea7622
Paola Toro <paotoro94@gmail.com>
2020-03-02 00:12:15 -0300

    Update cards flip logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { CardComponent } from './card/card.component';
 
 import shuffleArray from './helpers/shuffle-array';
 
+const MAX_DISPLAYING_CARDS = 2;
+
 // each player have 1 minute to play
 const INTERVAL_TIME = 60 * 1000;
 
@@ -40,7 +42,9 @@ export class AppComponent {
 
   timer = null;
   time = '00:00';
+
   start = false;
+  disabled = false;
 
   player1 = 0;
   player2 = 0;
@@ -48,6 +52,8 @@ export class AppComponent {
   // The size of the `HEROES_ASSETS` should be the half of `rows` * `columns`
   rows = 5;
   columns = 4;
+
+  displayingCards = 0;
 
   matrix: Card[][];
 
@@ -98,5 +104,21 @@ export class AppComponent {
 
   startTimer() {
     this.timer = setTimeout(() => { console.log('time finish'); }, INTERVAL_TIME);
+  }
+
+  // Here we get the click from a bubble event of the card
+  handleClickOnCard(event: Event) {
+    event.stopPropagation();
+
+    if (this.start) {
+      this.displayingCards++;
+
+      if (this.displayingCards > 1) {
+        this.disabled = true;
+      }
+
+      // TODO: add the logic to decide what happen next
+      console.log('Click');
+    }
   }
 }
